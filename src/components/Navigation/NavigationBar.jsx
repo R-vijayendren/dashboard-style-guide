c43232a3eb28e7071db23d17f9038c75722adb7d
VijayendrenR <vijayendren@codebrahma.com>
2021-06-01 16:52:32 +0530

    Added nav bar more options component.

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navBar.scss';
 import { RiMiniProgramLine } from 'react-icons/ri';
 import { VscBellDot, VscMail, VscSearch } from 'react-icons/vsc';
 import { BsGrid3X3Gap } from 'react-icons/bs';
+import NavBarOptions from './NavBarOptions';
 
 const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='navBarWrapper'>
       <div className='appLogoContainer'>
@@ -19,9 +21,16 @@ const NavigationBar = () => {
           <VscBellDot size={20} />
           <VscMail size={20} />
           <VscSearch size={20} />
-          <BsGrid3X3Gap size={20} />
+          <div onClick={() => setIsOpen(!isOpen)}>
+            <BsGrid3X3Gap size={20} />
+          </div>
         </div>
       </div>
+      {isOpen && (
+        <div className='navOptionsContainer'>
+          <NavBarOptions />
+        </div>
+      )}
     </div>
   );
 };
