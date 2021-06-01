8acc9fb3b577a3de0288f7bcbd01c963ba18288d
VijayendrenR <vijayendren@codebrahma.com>
2021-06-02 01:58:26 +0530

    Removed the unwanted icons and added the new icons as per the options.

--- src/components/SideBar/helpers.js
+++ src/components/SideBar/helpers.js
@@ -1,33 +1,50 @@
 import './SideBar.scss';
 
+import { BsFillHouseDoorFill, BsTextLeft } from 'react-icons/bs';
+import { AiFillThunderbolt } from 'react-icons/ai';
 import {
-  FaUserAlt,
-  FaCoins,
-  FaRegCalendarAlt,
-  FaRegLifeRing
-} from 'react-icons/fa';
-import { GoGlobe } from 'react-icons/go';
-import { GiThreeFriends } from 'react-icons/gi';
+  IoLogoWebComponent,
+  IoSettingsSharp,
+  IoDocumentsSharp
+} from 'react-icons/io5';
+import { VscMail } from 'react-icons/vsc';
+import { ImCopy } from 'react-icons/im';
+import { FaWpforms, FaMap, FaHtml5 } from 'react-icons/fa';
 
 const renderIcon = value => {
   switch (value) {
-    case 'profile': {
-      return <FaUserAlt size={15} />;
+    case 'dashboard': {
+      return <BsFillHouseDoorFill size={13} />;
     }
-    case 'balance': {
-      return <FaCoins size={15} />;
+    case 'apps': {
+      return <AiFillThunderbolt size={13} />;
     }
-    case 'connections': {
-      return <GoGlobe size={15} />;
+    case 'components': {
+      return <IoLogoWebComponent size={13} />;
     }
-    case 'friends': {
-      return <GiThreeFriends size={15} />;
+    case 'email': {
+      return <VscMail size={13} />;
     }
-    case 'events': {
-      return <FaRegCalendarAlt size={15} />;
+    case 'form': {
+      return <FaWpforms size={13} />;
     }
-    case 'account': {
-      return <FaRegLifeRing size={15} />;
+    case 'maps': {
+      return <FaMap size={13} />;
+    }
+    case 'pages': {
+      return <ImCopy size={13} />;
+    }
+    case 'menu': {
+      return <BsTextLeft size={13} />;
+    }
+    case 'settings': {
+      return <IoSettingsSharp size={13} />;
+    }
+    case 'document': {
+      return <IoDocumentsSharp size={13} />;
+    }
+    case 'version': {
+      return <FaHtml5 size={13} />;
     }
     default:
       return null;
