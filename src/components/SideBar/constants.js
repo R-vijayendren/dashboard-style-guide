13e50cee04066a4e8d35853964ef3d385175aabb
VijayendrenR <vijayendren@codebrahma.com>
2021-06-02 01:23:09 +0530

    Updated the options constant with specfic color code

--- src/components/SideBar/constants.js
+++ src/components/SideBar/constants.js
@@ -1,16 +1,16 @@
 export const SIDE_BAR_OPTIONS = [
-  { key: 'dashboard', label: 'Dashboard', count: null },
-  { key: 'apps', label: 'Apps', count: 3 },
-  { key: 'components', label: 'Components', count: 9 },
-  { key: 'email', label: 'Email', count: 4 },
-  { key: 'form', label: 'Form', count: null },
-  { key: 'maps', label: 'Maps', count: null },
-  { key: 'pages', label: 'Pages', count: 8 },
-  { key: 'menu', label: 'Menu', count: null }
+  { key: 'dashboard', label: 'Dashboard', count: null, badgeColor: '#a32cf8' },
+  { key: 'apps', label: 'Apps', count: 3, badgeColor: '#fd7004' },
+  { key: 'components', label: 'Components', count: 9, badgeColor: '#36b61c' },
+  { key: 'email', label: 'Email', count: 4, badgeColor: '#5704fd' },
+  { key: 'form', label: 'Form', count: null, badgeColor: '#a32cf8' },
+  { key: 'maps', label: 'Maps', count: null, badgeColor: '#a32cf8' },
+  { key: 'pages', label: 'Pages', count: 8, badgeColor: '#fdc304' },
+  { key: 'menu', label: 'Menu', count: null, badgeColor: '#a32cf8' }
 ];
 
 export const SIDE_BAR_GLOBAL_OPTIONS = [
-  { key: 'settings', label: 'Settings', count: null },
-  { key: 'document', label: 'Document', count: 4 },
-  { key: 'version', label: 'HTML Version', count: null }
+  { key: 'settings', label: 'Settings', count: null, badgeColor: '#a32cf8' },
+  { key: 'document', label: 'Document', count: 4, badgeColor: '#040cfd' },
+  { key: 'version', label: 'HTML Version', count: null, badgeColor: '#a32cf8' }
 ];
