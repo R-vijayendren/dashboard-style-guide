26d089997863b38c84f63e15ae276ca0534f1b2e
VijayendrenR <vijayendren@codebrahma.com>
2021-06-02 14:30:06 +0530

    Added the actual card data and styles to the dashboard content.

--- src/pages/Dashboard/DashboardContent.jsx
+++ src/pages/Dashboard/DashboardContent.jsx
@@ -6,8 +6,26 @@ const DashboardContent = () => {
   return (
     <div className='dashboard-content-wrapper'>
       <div className='dashboard-welcome-container'>
-        <Card>Dashboard item</Card>
-        <Card>Dashboard item</Card>
+        <Card>
+          <div className='card-item-container'>
+            <div className='card-left-content'>
+              <div className='card-label'>
+                Welcome to <em>New Space</em>
+              </div>
+              <div className='card-description'>Number of Views</div>
+            </div>
+            <div className='card-right-content'>102</div>
+          </div>
+        </Card>
+        <Card>
+          <div className='card-item-container'>
+            <div className='card-left-content'>
+              <div className='card-label'>Active</div>
+              <div className='card-description'>Leads/Contacts</div>
+            </div>
+            <div className='card-right-content'>25</div>
+          </div>
+        </Card>
       </div>
     </div>
   );
