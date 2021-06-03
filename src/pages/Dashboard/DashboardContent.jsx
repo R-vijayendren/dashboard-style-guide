2531822383778dca65e58520796735d886aa7303
VijayendrenR <vijayendren@codebrahma.com>
2021-06-03 10:03:07 +0530

    Added the leaderboard data and its styles..

--- src/pages/Dashboard/DashboardContent.jsx
+++ src/pages/Dashboard/DashboardContent.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
+import { DASHBOARD_LEADERBOARD_DATA } from './constants';
 import './Dashboard.scss';
 
+import { FiPaperclip, FiClipboard } from 'react-icons/fi';
+import { BiLockAlt, BiBlock } from 'react-icons/bi';
+import { RiBarChart2Fill } from 'react-icons/ri';
+
 const DashboardContent = () => {
+  const renderIcon = value => {
+    switch (value) {
+      case 'leads': {
+        return <FiPaperclip size={40} />;
+      }
+      case 'pending': {
+        return <BiBlock size={40} />;
+      }
+      case 'closed': {
+        return <BiLockAlt size={40} />;
+      }
+      case 'confirmed': {
+        return <FiClipboard size={40} />;
+      }
+      default: {
+        return null;
+      }
+    }
+  };
+
+  const renderLeaderBoard = leaderBoard => {
+    return (
+      <Card key={leaderBoard.key}>
+        <div className='card-name'>
+          <div className='card-label'>
+            <div className='card-count'>{leaderBoard.count}</div>
+            <div className='card-label__name'>{leaderBoard.label}</div>
+          </div>
+          <div className='card-icon'>{renderIcon(leaderBoard.key)}</div>
+        </div>
+        <div
+          style={{ backgroundColor: leaderBoard.statColor }}
+          className='card-footer'
+        >
+          <div>% Change</div>
+          <div>
+            <RiBarChart2Fill size={20} />
+          </div>
+        </div>
+      </Card>
+    );
+  };
+
   return (
     <div className='dashboard-content-wrapper'>
       <div className='dashboard-welcome-container'>
@@ -26,6 +74,11 @@ const DashboardContent = () => {
             <div className='card-right-content'>25</div>
           </div>
         </Card>
+      </div>
+      <div className='leaderboard-container'>
+        {DASHBOARD_LEADERBOARD_DATA.map(leaderboardItem =>
+          renderLeaderBoard(leaderboardItem)
+        )}
       </div>
     </div>
   );
