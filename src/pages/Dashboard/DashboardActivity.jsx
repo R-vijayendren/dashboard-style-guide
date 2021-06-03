d93bf06b52abab00a1cbfae065b06c0330381873
VijayendrenR <vijayendren@codebrahma.com>
2021-06-03 20:31:34 +0530

    Added pie chart and activity cards to the dashboard.

---
+++ src/pages/Dashboard/DashboardActivity.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Cell, Legend, Pie, PieChart } from 'recharts';
+import Card from '../../components/Card/Card';
+
+const data = [
+  { name: 'Group A', value: 400 },
+  { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 }
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
+
+const DashboardActivity = () => {
+  return (
+    <div className='card-pie-chart-container'>
+      <Card>
+        <div className='card-head__header'>ACTIVITIES</div>
+        <div className='pie-chart-container'>
+          <PieChart width={100} height={100}>
+            <Pie
+              data={data}
+              cx='50%'
+              cy='50%'
+              labelLine={false}
+              outerRadius={50}
+              fill='#8884d8'
+              dataKey='value'
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          dolorum rem tenetur quibusdam, blanditiis expedita voluptatem?
+          Similique veritatis nihil molestiae!
+        </div>
+      </Card>
+      <Card>
+        <div className='card-head__header'>ACTIVITIES</div>
+        <div className='pie-chart-container'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+          dolorum rem tenetur quibusdam, blanditiis expedita voluptatem?
+          Similique veritatis nihil molestiae!
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default DashboardActivity;
--- src/pages/Dashboard/DashboardContent.jsx
+++ src/pages/Dashboard/DashboardContent.jsx
@@ -7,6 +7,7 @@ import { FiPaperclip, FiClipboard } from 'react-icons/fi';
 import { BiLockAlt, BiBlock } from 'react-icons/bi';
 import { RiBarChart2Fill } from 'react-icons/ri';
 import CardWithChart from './CardWithChart';
+import DashboardActivity from './DashboardActivity';
 
 const DashboardContent = () => {
   const renderIcon = value => {
@@ -87,6 +88,7 @@ const DashboardContent = () => {
         )}
       </div>
       <div className='card-activity-container'>
+        <DashboardActivity />
         <CardWithChart />
       </div>
     </div>
