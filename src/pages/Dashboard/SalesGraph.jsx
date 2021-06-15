af79994289bdf2a279334648035211e15482f7a0
VijayendrenR <vijayendren@codebrahma.com>
2021-06-15 23:44:20 +0530

    Added the sales chart and customized the chart line.

--- src/pages/Dashboard/SalesGraph.jsx
+++ src/pages/Dashboard/SalesGraph.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
 
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+
 import './Dashboard.scss';
+
+import { SALES_GRAPH_DATA } from './constants';
 
 const SalesGraph = () => {
   return (
@@ -14,7 +18,36 @@ const SalesGraph = () => {
           expedita maxime vel aut.
         </div>
       </div>
-      <div className='sales-graph'></div>
+      <div className='sales-graph'>
+        <LineChart width={560} height={280} data={SALES_GRAPH_DATA}>
+          <defs>
+            <linearGradient id='gradient' x1='0' y1='0' x2='100%' y2='0'>
+              <stop offset='0%' stopColor='red' />
+              <stop offset='25%' stopColor='#387b30' />
+              <stop offset='50%' stopColor='#2afc9e' />
+              <stop offset='75%' stopColor='#1b23fh' />
+              <stop offset='100%' stopColor='blue' />
+            </linearGradient>
+          </defs>
+          <XAxis
+            dataKey='name'
+            tickMargin={20}
+            tickSize={0}
+            padding={{ left: -10 }}
+            tick={{ fill: '#888' }}
+            stroke='#d3d3d3'
+          />
+          <YAxis tickSize={0} tick={0} stroke='#d3d3d3' />
+          <Line
+            type='monotone'
+            dataKey='customers'
+            stroke='url(#gradient)'
+            strokeWidth={5}
+            dot={false}
+            strokeLinecap='round'
+          />
+        </LineChart>
+      </div>
     </Card>
   );
 };
--- src/pages/Dashboard/constants.js
+++ src/pages/Dashboard/constants.js
@@ -88,6 +88,54 @@ export const CUSTOMER_USER_DATA = [
   }
 ];
 
+export const SALES_GRAPH_DATA = [
+  {
+    name: 'Jan',
+    customers: 340,
+    users: 240
+  },
+  {
+    name: 'Feb',
+    customers: 400,
+    users: 200
+  },
+  {
+    name: 'Mar',
+    customers: 300,
+    users: 400
+  },
+  {
+    name: 'Apr',
+    customers: 420,
+    users: 300
+  },
+  {
+    name: 'May',
+    customers: 470,
+    users: 280
+  },
+  {
+    name: 'Jun',
+    customers: 360,
+    users: 100
+  },
+  {
+    name: 'Jul',
+    customers: 260,
+    users: 600
+  },
+  {
+    name: 'Aug',
+    customers: 290,
+    users: 370
+  },
+  {
+    name: 'Sep',
+    customers: 674,
+    users: 328
+  }
+];
+
 export const DASHBOARD_TODO_LIST_DATA = [
   {
     id: 1,
