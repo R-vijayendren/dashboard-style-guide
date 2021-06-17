01c40b2758064ac47bcf789e956c1bd09b55db8f
VijayendrenR <vijayendren@codebrahma.com>
2021-06-17 13:51:04 +0530

    fixed the graph spacing and time-line spacing issues in activity container

--- src/pages/Dashboard/CardWithChart.jsx
+++ src/pages/Dashboard/CardWithChart.jsx
@@ -9,9 +9,16 @@ const CardWithChart = () => {
     <Card className='card-chart-container'>
       <div className='card-head__header'>ANALYTICS</div>
       <div className='chart-container'>
-        <BarChart width={420} height={300} data={CUSTOMER_USER_DATA}>
-          <XAxis dataKey='name' tickMargin={20} axisLine={false} tickSize={0} />
+        <BarChart width={530} height={350} data={CUSTOMER_USER_DATA}>
+          <XAxis
+            dataKey='name'
+            tickMargin={20}
+            axisLine={false}
+            tickSize={0}
+            tick={{ fill: '#888', fontSize: '12px' }}
+          />
           <YAxis
+            tick={{ fill: '#888', fontSize: '12px' }}
             tickMargin={20}
             tickSize={0}
             interval={0}
@@ -21,7 +28,7 @@ const CardWithChart = () => {
           <Legend
             width={80}
             wrapperStyle={{
-              top: 10,
+              top: 0,
               left: 100,
               backgroundColor: '#fff',
               border: '1px solid #d3d3d3',
