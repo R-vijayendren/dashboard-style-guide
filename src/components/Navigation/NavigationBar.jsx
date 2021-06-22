8259863677269f548f6d497c7326452dc6ec6541
VijayendrenR <vijayendren@codebrahma.com>
2021-06-22 16:43:21 +0530

    Redesigned the nav bar styles and items container.

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -4,9 +4,11 @@ import { RiMiniProgramLine } from 'react-icons/ri';
 import { VscBellDot, VscMail, VscSearch } from 'react-icons/vsc';
 import { BsGrid3X3Gap } from 'react-icons/bs';
 import NavBarOptions from './NavBarOptions';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const NavigationBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const windowSize = useWindowSize();
   return (
     <div className='navBarWrapper'>
       <div className='appLogoContainer'>
@@ -18,15 +20,19 @@ const NavigationBar = () => {
       <div className='navItemContainer'>
         <div className='pageTitle'>Dashboard</div>
         <div className='navItems'>
-          <div className='nav-icons'>
-            <VscBellDot size={20} />
-          </div>
-          <div className='nav-icons'>
-            <VscMail size={20} />
-          </div>
-          <div className='nav-icons'>
-            <VscSearch size={20} />
-          </div>
+          {windowSize.width > 768 && (
+            <div>
+              <div className='nav-icons'>
+                <VscBellDot size={20} />
+              </div>
+              <div className='nav-icons'>
+                <VscMail size={20} />
+              </div>
+              <div className='nav-icons'>
+                <VscSearch size={20} />
+              </div>
+            </div>
+          )}
           <div className='nav-icons' onClick={() => setIsOpen(!isOpen)}>
             <BsGrid3X3Gap size={20} />
           </div>
